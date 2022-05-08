c57487654b8e353315101b3357ca710ccae6d6de
Cleyton A Silva <cleyton.eu123@gmail.com>
2022-05-08 02:27:37 -0300

    Adicionando terreno que corre

--- dino.c
+++ dino.c
@@ -5,28 +5,22 @@
 
 // Limpar a tela.
 #define LIMPA_TELA system("cls");
+#define ESPERA Sleep(1);
 
-// Limite da Tela em colunas de caracteres.
-int limiteInferiorY = 32;
+// Variáveis para controle da tela lógica.
+int limiteInferiorY = 12;
 int limiteSuperiorY = 2;
 int limiteInferiorX = 2;
 int limiteSuperiorX = 72;
-
+int nColunas;
+int alturaTerreno = 9;
+float tempoAtualizacao = 6000;
+float fracaoTempo = 0.001;
 char caractereBorda = '*';
 
 // Teclas.
 int ENTER = 13;
 int CIMA = 72;
-
-// Posiciona cursor para escrita subsequente.
-void setCursor(int x, int y)
-{
-    COORD coord; // Struct para definir as coordenadas de um caractere no buffer do console.
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), // Buffer da tela do console.
-                             coord);                          // Nova coordenada.
-}
 
 // Struct para guardar informações dos obstáculos.
 typedef struct Bloco
@@ -38,49 +32,64 @@ typedef struct Bloco
     void (*moverBloco)(struct Bloco *, int novoX, int novoY);
 } Bloco;
 
-// Função para mudar a posição do bloco no terminal.
+// Classe utilizada para desenho do chão;
+typedef struct Terreno
+{
+    char *terreno;
+    int inicioAatual;
+    int tamanhoTerreno;
+} Terreno;
+
+// Funções utilizadas
+void setCursor(int x, int y);
 void moverBloco(Bloco *bloco, int novoX, int novoY);
-
-// Função para desenhar a borda.
 void desenharBorda();
+void moverTerreno(Terreno *terreno);
+Bloco *criarBloco(int x, int y, int tamanho);
 
-Bloco *criarBloco(int x, int y, int tamanho)
+int main()
 {
-    Bloco *bloco = (Bloco *)malloc(sizeof(Bloco));
-    bloco->x = x;
-    bloco->y = y;
-    bloco->tamanho = tamanho;
-    bloco->moverBloco = &moverBloco; // Definindo o pseudométodo.
-    return bloco;
-}
-
-// Funções utilizadas.
-void
-
-main()
-{
+    // Iniciando variáveis dinâmicas
+    nColunas = 1 + limiteSuperiorX - limiteInferiorX;
     LIMPA_TELA;
     desenharBorda();
+    Terreno terreno;
+    terreno.terreno = "-.^-----.--.---__------.^.---------~~--^--------...__-.--------------^~---";
+    terreno.inicioAatual = 0;
+    terreno.tamanhoTerreno = 74;
+    float velocidade = 10; // Posteriormente variável com a dificuldade.
+
     // Criar tela inicial.
 
     // Capturar evento para iniciar a partida.
 
     // Criar os blocos.
-    Bloco blocos[3] = {*criarBloco(0, 0, 1),
-                       *criarBloco(0, 10, 2),
-                       *criarBloco(0, 20, 3)};
+    Bloco blocos[3] = {*criarBloco(nColunas / 6, alturaTerreno - 1, 1),
+                       *criarBloco(nColunas / 2, alturaTerreno - 1, 2),
+                       *criarBloco(nColunas / 3, alturaTerreno - 1, 3)};
     for (int i = 0; i < 3; i++)
     {
         Bloco bloco = blocos[i];
-        // bloco.moverBloco(&bloco, bloco.x, bloco.y);
+        bloco.moverBloco(&bloco, bloco.x, bloco.y);
     }
 
     // Mover os blocos.
 
-    // fim
-    while (1)
+    // Mover terreno.
+    int nivel = 1;
+    for (float i = 0; 1; i += fracaoTempo)
     {
+        if (i > (velocidade/nivel) * tempoAtualizacao)
+        {
+            moverTerreno(&terreno);
+            setCursor(limiteInferiorX, limiteInferiorY + 2);
+        }
+        if(i >tempoAtualizacao){
+            nivel++;
+        }
     }
+
+    // fim
 }
 
 void moverBloco(Bloco *bloco, int novoX, int novoY)
@@ -101,8 +110,6 @@ void moverBloco(Bloco *bloco, int novoX, int novoY)
 // Desenha a borda (exterior) da tela lógica.
 void desenharBorda()
 {
-    int nColunas = 1 + limiteSuperiorX - limiteInferiorX;
-
     // Desenho das linhas horizontais.
     char *str = malloc((nColunas + 1) * sizeof(char));
     // Cria a string com o caractere de borda.
@@ -119,7 +126,7 @@ void desenharBorda()
     printf("%s", str);
 
     // Desenho das linhas verticais (Acompanha quinas).
-    for (int i = limiteSuperiorY - 1; i <= limiteInferiorY+1; i++)
+    for (int i = limiteSuperiorY - 1; i <= limiteInferiorY + 1; i++)
     {
         // Borda vertical esquerda.
         setCursor(limiteInferiorX - 1, i);
@@ -130,4 +137,37 @@ void desenharBorda()
     }
 
     printf("\n \n");
+}
+
+// Posiciona cursor para escrita subsequente.
+void setCursor(int x, int y)
+{
+    COORD coord; // Struct para definir as coordenadas de um caractere no buffer do console.
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), // Buffer da tela do console.
+                             coord);                          // Nova coordenada.
+}
+
+Bloco *criarBloco(int x, int y, int tamanho)
+{
+    Bloco *bloco = (Bloco *)malloc(sizeof(Bloco));
+    bloco->x = x;
+    bloco->y = y;
+    bloco->tamanho = tamanho;
+    bloco->moverBloco = &moverBloco; // Definindo o pseudométodo.
+    return bloco;
+}
+
+void moverTerreno(Terreno *terreno)
+{
+    // Posiciona o cursos na posição do terreno.
+    setCursor(limiteInferiorX, alturaTerreno);
+    terreno->inicioAatual = terreno->inicioAatual < (terreno->tamanhoTerreno - 1) ? terreno->inicioAatual + 1 : 0;
+    int inicio = terreno->inicioAatual;
+    for (int i = 0; i < nColunas; i++)
+    {
+        printf("%c", terreno->terreno[inicio]);
+        inicio = inicio < (terreno->tamanhoTerreno - 1) ? inicio + 1 : 0;
+    }
 }
